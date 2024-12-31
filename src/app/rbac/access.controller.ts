e1aeab46d226d4e5817669c6a364b365aaab6419
Faizan AHmed - DAX <faizana@digitalauxilius.com>
2024-12-31 11:44:50 +0500

    Dec 31 2024

--- src/app/rbac/access.controller.ts
+++ src/app/rbac/access.controller.ts
@@ -74,49 +74,6 @@ export class AccessController {
         }
     )
 
-    // signupDriver = asyncHandler(
-    //   async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
-    //     const user = await UserRepo.findByEmail(req.body.email);
-    //     if (user) throw new BadRequestError('User already registered');
-    //     // if (user && user.email) throw new BadRequestError('User already registered');
-
-    //     const accessTokenKey = generateTokenKey();
-    //     const refreshTokenKey = generateTokenKey();
-
-    //     req.body.referCode = await generateCode(5, 0, 70, '')
-
-    //     const { user: createdUser, keystore } = await UserRepo.create(
-    //       req.body as User,
-    //       accessTokenKey,
-    //       refreshTokenKey,
-    //       "DRIVER",
-    //     );
-
-    //     if (!createdUser) throw new BadRequestError('User creation field!');
-    //     const tokens = await createTokens(createdUser, keystore.primaryKey, keystore.secondaryKey);
-
-    //     const { token } = await this.tokenService.createToken({
-    //       shot_code: generateOTP(),
-    //       token: generateTokenKey(),
-    //       type: 'PHONE_VERIFY',
-    //       userId: createdUser.id,
-    //       expireAt: new Date()
-    //     } as Token)
-
-    //     // let link = `http://52.192.208.76/api/v1/auth/email-verify?token=${token?.token}&user=${token?.userId}`
-    //     // if (createdUser && createdUser.email) {
-    //     //   // @ts-ignore
-    //     //   sendMail({ to: createdUser.email, text: link })
-    //     // }
-    //     console.log("Token: ", token)
-    //     new SuccessResponse('Signup Successful', {
-    //       user: _.pick(createdUser, ['id', 'first_name', 'last_name', 'email', 'phone_status', 'role', 'profilePicUrl', 'gender']),
-    //       tokens,
-    //       token
-    //     }).send(res);
-    //   }
-    // )
-
     signin = asyncHandler(
         async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
 
@@ -164,45 +121,45 @@ export class AccessController {
     )
 
 
-    getMe = asyncHandler(
-        async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
+    // getMe = asyncHandler(
+    //     async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
 
-            const user = await UserRepo.findById(req.user.id);
-            new SuccessResponse('fetch success', { user }).send(res);
+    //         const user = await UserRepo.findById(req.user.id);
+    //         new SuccessResponse('fetch success', { user }).send(res);
 
-        }
-    )
+    //     }
+    // )
 
-    getUserById = asyncHandler(
-        async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
+    // getUserById = asyncHandler(
+    //     async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
 
-            const user = await UserRepo.findById(req.params.id);
-            new SuccessResponse('fetch success', { user }).send(res);
+    //         const user = await UserRepo.findById(req.params.id);
+    //         new SuccessResponse('fetch success', { user }).send(res);
 
-        }
-    )
+    //     }
+    // )
 
-    addUser = asyncHandler(
-        async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
-            const user = await UserRepo.findByEmail(req.body.email);
-            if (user) throw new BadRequestError('User already registered');
+    // addUser = asyncHandler(
+    //     async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
+    //         const user = await UserRepo.findByEmail(req.body.email);
+    //         if (user) throw new BadRequestError('User already registered');
 
-            const { user: createdUser } = await UserRepo.createUser({
-                ...req.body,
-                // companyId: req.company.id,
-                phone_status: "PENDING"
-            });
-            new SuccessResponse('fetch success', { user: createdUser }).send(res);
+    //         const { user: createdUser } = await UserRepo.createUser({
+    //             ...req.body,
+    //             // companyId: req.company.id,
+    //             phone_status: "PENDING"
+    //         });
+    //         new SuccessResponse('fetch success', { user: createdUser }).send(res);
 
-        }
-    )
+    //     }
+    // )
 
-    getUsers = asyncHandler(
-        async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
-            const users = await UserRepo.findUsers();
-            new SuccessResponse('fetch success', { users }).send(res);
-        }
-    )
+    // getUsers = asyncHandler(
+    //     async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
+    //         const users = await UserRepo.findUsers();
+    //         new SuccessResponse('fetch success', { users }).send(res);
+    //     }
+    // )
 
     // getTeachers = asyncHandler(
     //   async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
@@ -211,30 +168,30 @@ export class AccessController {
     //   }
     // )
 
-    updateUser = asyncHandler(
-        async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
-            const { body, params } = req;
-            const user = await UserRepo.update(params.id, body);
-            new SuccessResponse('update success', { user }).send(res);
-        }
-    )
+    // updateUser = asyncHandler(
+    //     async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    //         const { body, params } = req;
+    //         const user = await UserRepo.update(params.id, body);
+    //         new SuccessResponse('update success', { user }).send(res);
+    //     }
+    // )
 
-    updateMe = asyncHandler(
-        async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
-            const { body } = req;
-            const users = req.user.id;
-            const user = await UserRepo.update(users, body);
-            new SuccessResponse('update success', { user }).send(res);
-        }
-    )
+    // updateMe = asyncHandler(
+    //     async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
+    //         const { body } = req;
+    //         const users = req.user.id;
+    //         const user = await UserRepo.update(users, body);
+    //         new SuccessResponse('update success', { user }).send(res);
+    //     }
+    // )
 
-    delete = asyncHandler(
-        async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
-            const { params } = req;
-            const user = await UserRepo.delete(params.id);
-            new SuccessResponse('delete success', { user }).send(res);
-        }
-    )
+    // delete = asyncHandler(
+    //     async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    //         const { params } = req;
+    //         const user = await UserRepo.delete(params.id);
+    //         new SuccessResponse('delete success', { user }).send(res);
+    //     }
+    // )
 
     // verifyEmail = asyncHandler(
     //   async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
@@ -250,31 +207,31 @@ export class AccessController {
     //   }
     // )
 
-    verifyPhone = asyncHandler(
-        async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
-            const user = req.user.id;
-            const { otp } = req.body;
-            req.accessToken = getAccessToken(req.headers.authorization);
+    // verifyPhone = asyncHandler(
+    //     async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
+    //         const user = req.user.id;
+    //         const { otp } = req.body;
+    //         req.accessToken = getAccessToken(req.headers.authorization);
 
-            if (!_.isString(user) || !_.isString(otp)) throw new BadRequestError('Invalid Request')
-            //@ts-ignore
-            const otpfound = await this.tokenService.findForPhoneVerification({ otp, user })
-            if (!otpfound) throw new BadRequestError('OTP Expire') //  || tokenfound.expireAt < new Date()
+    //         if (!_.isString(user) || !_.isString(otp)) throw new BadRequestError('Invalid Request')
+    //         //@ts-ignore
+    //         const otpfound = await this.tokenService.findForPhoneVerification({ otp, user })
+    //         if (!otpfound) throw new BadRequestError('OTP Expire') //  || tokenfound.expireAt < new Date()
 
-            const accessTokenKey = generateTokenKey();
-            const refreshTokenKey = generateTokenKey();
+    //         const accessTokenKey = generateTokenKey();
+    //         const refreshTokenKey = generateTokenKey();
 
-            const userData = await UserRepo.update(user, { phone_status: "VERIFIED" } as User);
-            await KeystoreRepo.create(user, accessTokenKey, refreshTokenKey);
-            // @ts-ignore
-            const tokens = await createTokens(userData, accessTokenKey, refreshTokenKey);
-            new SuccessResponse('otp success', {
-                user: _.pick(userData, ['id', 'first_name', 'last_name', 'email', 'phone_status', 'profile_picture', 'role', 'profilePicUrl', 'gender']),
-                tokens: tokens
-            }).send(res);
-            // res.redirect('https://elect.com/')
-        }
-    )
+    //         const userData = await UserRepo.update(user, { phone_status: "VERIFIED" } as User);
+    //         await KeystoreRepo.create(user, accessTokenKey, refreshTokenKey);
+    //         // @ts-ignore
+    //         const tokens = await createTokens(userData, accessTokenKey, refreshTokenKey);
+    //         new SuccessResponse('otp success', {
+    //             user: _.pick(userData, ['id', 'first_name', 'last_name', 'email', 'phone_status', 'profile_picture', 'role', 'profilePicUrl', 'gender']),
+    //             tokens: tokens
+    //         }).send(res);
+    //         // res.redirect('https://elect.com/')
+    //     }
+    // )
 
     // signout = asyncHandler(
     //   async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
@@ -337,81 +294,81 @@ export class AccessController {
 
     //   }
     // )
-    verifyEmail = asyncHandler(
-        async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
-            const user = await UserRepo.findByEmail(req.body.email);
-            if (user) throw new BadRequestError('User already registered');
+    // verifyEmail = asyncHandler(
+    //     async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
+    //         const user = await UserRepo.findByEmail(req.body.email);
+    //         if (user) throw new BadRequestError('User already registered');
 
-            new SuccessMsgResponse('Email Not Found')
-        }
-    )
+    //         new SuccessMsgResponse('Email Not Found')
+    //     }
+    // )
 
-    forgotPassword = asyncHandler(
-        async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
+    // forgotPassword = asyncHandler(
+    //     async (req: any, res: Response, next: NextFunction): Promise<Response | void> => {
 
-            const user = await UserRepo.findByEmail(req.body.email);
-            if (!user || !user.email) throw new SuccessMsgResponse('Email send success, Check your email').send(res);
+    //         const user = await UserRepo.findByEmail(req.body.email);
+    //         if (!user || !user.email) throw new SuccessMsgResponse('Email send success, Check your email').send(res);
 
-            const { token } = await this.tokenService.createToken({
-                shot_code: generateTokenKey(5),
-                token: generateTokenKey(),
-                type: 'FORGOT_PASSWORD',
-                userId: user.id,
-                expireAt: new Date()
-            } as Token)
+    //         const { token } = await this.tokenService.createToken({
+    //             shot_code: generateTokenKey(5),
+    //             token: generateTokenKey(),
+    //             type: 'FORGOT_PASSWORD',
+    //             userId: user.id,
+    //             expireAt: new Date()
+    //         } as Token)
 
-            let link = `http://localhost:5001/api/v1/auth/reset-password/?token=${token?.token}&user=${token?.userId}&email=${user.email}`
-            if (user && user.email) {
-                // @ts-ignore
-                // sendMail({ subject: 'iPrint (Forgot Password)', to: user.email, text: link })
-            }
-            console.log("==== link ====", link);
+    //         let link = `http://localhost:5001/api/v1/auth/reset-password/?token=${token?.token}&user=${token?.userId}&email=${user.email}`
+    //         if (user && user.email) {
+    //             // @ts-ignore
+    //             // sendMail({ subject: 'iPrint (Forgot Password)', to: user.email, text: link })
+    //         }
+    //         console.log("==== link ====", link);
 
-            new SuccessMsgResponse('Email send success, Check your email').send(res);
-        }
-    )
+    //         new SuccessMsgResponse('Email send success, Check your email').send(res);
+    //     }
+    // )
 
-    resetPassword = asyncHandler(
-        async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
-            const { token, otp } = req.query;
+    // resetPassword = asyncHandler(
+    //     async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    //         const { token, otp } = req.query;
 
-            if (!Boolean(_.isString(token) || _.isString(otp))) throw new BadRequestError('Invalid Request')
-            const tokenfound = await this.tokenService.verifyCode({ code: token as string, type: 'FORGOT_PASSWORD' })
+    //         if (!Boolean(_.isString(token) || _.isString(otp))) throw new BadRequestError('Invalid Request')
+    //         const tokenfound = await this.tokenService.verifyCode({ code: token as string, type: 'FORGOT_PASSWORD' })
 
-            await UserRepo.updatePassword(tokenfound.userId, { password: req.body.password });
-            new SuccessMsgResponse('password reset Successful').send(res);
-        }
-    )
+    //         await UserRepo.updatePassword(tokenfound.userId, { password: req.body.password });
+    //         new SuccessMsgResponse('password reset Successful').send(res);
+    //     }
+    // )
 
-    passwordOTPVerify = asyncHandler(
-        async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
-            const { otp, email } = req.body;
-            const user = await UserRepo.findByEmail(email);
-            if (!user) throw new BadRequestError('User not exist');
+    // passwordOTPVerify = asyncHandler(
+    //     async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    //         const { otp, email } = req.body;
+    //         const user = await UserRepo.findByEmail(email);
+    //         if (!user) throw new BadRequestError('User not exist');
 
-            const userId = user.id
+    //         const userId = user.id
 
-            if (!_.isString(userId) || !_.isString(otp)) throw new BadRequestError('Invalid Request')
-            //@ts-ignore
-            const otpfound = await this.tokenService.findForPasswordVerification({ otp, userId })
-            if (!otpfound) throw new BadRequestError('OTP Expire') //  || tokenfound.expireAt < new Date()
+    //         if (!_.isString(userId) || !_.isString(otp)) throw new BadRequestError('Invalid Request')
+    //         //@ts-ignore
+    //         const otpfound = await this.tokenService.findForPasswordVerification({ otp, userId })
+    //         if (!otpfound) throw new BadRequestError('OTP Expire') //  || tokenfound.expireAt < new Date()
 
-            new SuccessResponse('otp success', { user: userId }).send(res);
-        }
-    )
+    //         new SuccessResponse('otp success', { user: userId }).send(res);
+    //     }
+    // )
 
-    verifyPassword = asyncHandler(
-        async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
-            const { user } = req.query;
+    // verifyPassword = asyncHandler(
+    //     async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    //         const { user } = req.query;
 
-            // if (!_.isString(user) || _.isString(otp)) throw new BadRequestError('Invalid Request')
-            //@ts-ignore
+    //         // if (!_.isString(user) || _.isString(otp)) throw new BadRequestError('Invalid Request')
+    //         //@ts-ignore
 
-            await UserRepo.update(user, { password: req.body.password } as User);
-            new SuccessMsgResponse('password update success').send(res);
+    //         await UserRepo.update(user, { password: req.body.password } as User);
+    //         new SuccessMsgResponse('password update success').send(res);
 
-        }
-    )
+    //     }
+    // )
 
 
 
---
+++ src/app/rbac/access.routes.ts
@@ -0,0 +1,177 @@
+import { Router } from 'express';
+import validator from '../../../helpers/validator';
+import { signupSchema, userCredential } from "../../../utils/joi.schema";
+import { AccessController } from './access.controller';
+
+export class AccessRoutes {
+
+    readonly router: Router = Router();
+    readonly controller: AccessController = new AccessController()
+
+    constructor() {
+        this.initRoutes();
+    }
+
+    initRoutes(): void {
+
+        this.router.post(
+            '/signup',
+            validator(signupSchema),
+            this.controller.signup
+        )
+
+        // this.router.post(
+        //   '/driver/signup',
+        //   validator(driverSignupSchema),
+        //   this.controller.signupDriver
+        // )
+
+        this.router.post(
+            '/signin',
+            validator(userCredential),
+            this.controller.signin
+        )
+
+        // this.router.get(
+        //     '/me',
+        //     authentication,
+        //     // authorization(["ADMIN", "COMPANY_ADMIN", "ADMIN", "MANAGER", "ADMIN"]),
+        //     this.controller.getMe
+        // )
+
+        // this.router.post(
+        //     '/user',
+        //     authentication,
+        //     // authorization(["COMPANY_ADMIN"]),
+        //     // validator(employeeAddSchema),
+        //     this.controller.addUser
+        // )
+
+        // this.router.get(
+        //     '/users',
+        //     // authentication,
+        //     // authorization(["COMPANY_ADMIN", "ADMIN"]),
+        //     this.controller.getUsers
+        // )
+
+        // // this.router.get(
+        // //   '/email/verify',
+        // //   // authentication,
+        // //   // authorization(["COMPANY_ADMIN", "ADMIN"]),
+        // //   this.controller.emailVerify
+        // // )
+
+        // // this.router.get(
+        // //   '/teachers',
+        // //   // authentication,
+        // //   // authorization(["COMPANY_ADMIN", "ADMIN"]),
+        // //   this.controller.getTeachers
+        // // )
+
+        // // this.router.get(
+        // //   '/refer/list',
+        // //   authentication,
+        // //   this.controller.getReferUser
+        // // )
+
+        // this.router.put(
+        //     '/users/:id',
+        //     authentication,
+        //     // authorization(["COMPANY_ADMIN", "ADMIN"]),
+        //     // validator(employeeUpdateSchema),
+        //     this.controller.updateUser
+        // )
+
+        // this.router.put(
+        //     '/update/me',
+        //     authentication,
+        //     // authorization(["COMPANY_ADMIN", "ADMIN"]),
+        //     // validator(employeeUpdateSchema),
+        //     this.controller.updateMe
+        // )
+
+        // this.router.delete(
+        //     '/users/:id',
+        //     authentication,
+        //     // authorization(["COMPANY_ADMIN", "ADMIN"]),
+        //     this.controller.delete
+        // )
+
+        // // this.router.get(
+        // //   '/email-verify',
+        // //   this.controller.verifyEmail
+        // // )
+
+        // this.router.post(
+        //     '/phone-verify',
+        //     authentication,
+        //     this.controller.verifyPhone
+        // )
+
+        // this.router.get(
+        //     '/users/:id',
+        //     authentication,
+        //     authorization([RoleEnum.ADMIN]),
+        //     this.controller.getUserById
+        // )
+
+        // this.router.get(
+        //     '/email-verify',
+        //     this.controller.verifyEmail
+        // )
+
+        // this.router.post(
+        //     '/forgot-password',
+        //     validator(schema.forgotPassword),
+        //     this.controller.forgotPassword
+        // )
+
+        // this.router.post(
+        //     '/reset-password',
+        //     validator(schema.resetPassword),
+        //     this.controller.resetPassword
+        // )
+
+
+        // // this.router.delete(
+        // //   '/signout',
+        // //   authentication,
+        // //   this.controller.signout
+        // // )
+
+        // // this.router.post(
+        // //   '/verify',
+        // //   authentication,
+        // //   this.controller.verify
+        // // )
+
+        // // this.router.post(
+        // //   '/refresh',
+        // //   authentication,
+        // //   validator(refreshToken),
+        // //   this.controller.refresh
+        // // )
+
+        // // this.router.post(
+        // //   '/forgot-password',
+        // //   // authentication,
+        // //   // validator(refreshToken),
+        // //   this.controller.forgotPassword
+        // // )
+
+        // this.router.post(
+        //     '/password-otp',
+        //     // authentication,
+        //     // validator(refreshToken),
+        //     this.controller.passwordOTPVerify
+        // )
+
+        // this.router.post(
+        //     '/password-verify',
+        //     validator(verifyPassword),
+        //     this.controller.verifyPassword
+        // )
+
+    }
+
+}
